f0377be807b4b1b30f28a7df10b45a85b36e55f7
Antariksh Alse <74238119+Int-coder@users.noreply.github.com>
2023-05-02 12:29:32 +0530

    Add: name change

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import UserContext from "../lib/UserContext";
 import { supabase, fetchUserRoles } from "../lib/Store";
 
-export default function SupabaseSlackClone({ Component, pageProps }) {
+export default function NextChat({ Component, pageProps }) {
   const [userLoaded, setUserLoaded] = useState(false);
   const [user, setUser] = useState(null);
   const [session, setSession] = useState(null);
